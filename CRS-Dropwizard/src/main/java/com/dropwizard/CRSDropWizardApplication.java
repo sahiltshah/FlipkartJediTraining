6a5e599952f73e59e8937a14ce30fdddeb41e168
Sahil Shah <sahil.shah@flipkart.com>
2021-04-13 09:18:31 +0400

    removed DropWizard

--- CRS-Dropwizard/src/main/java/com/dropwizard/CRSDropWizardApplication.java
+++ CRS-Dropwizard/src/main/java/com/dropwizard/CRSDropWizardApplication.java
@@ -4,7 +4,6 @@ package com.dropwizard;
  * Hello world!
  *
  */
-import com.flipkart.bean.Professor;
 import com.flipkart.controller.AdminController;
 import com.flipkart.controller.ProfessorController;
 import com.flipkart.controller.StudentController;
@@ -23,9 +22,13 @@ import org.slf4j.LoggerFactory;
  * Hello world!
  *
  */
-public class App extends Application<Configuration> {
-    private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+public class CRSDropWizardApplication extends Application<Configuration> {
+    private static final Logger LOGGER = LoggerFactory.getLogger(CRSDropWizardApplication.class);
 
+    /**
+     *
+     * To initialize any content if need be.
+     */
     @Override
     public void initialize(Bootstrap<Configuration> b) {
         LOGGER.info("initialization call");
@@ -38,24 +41,20 @@ public class App extends Application<Configuration> {
      */
     @Override
     public void run(Configuration c, Environment e) throws Exception {
-        System.out.println("Run call");
+        LOGGER.info("Run call");
         LOGGER.info("Registering REST resources");
-        //  e.jersey().register(new EmployeeRESTController(e.getValidator()));
-        System.out.println("Registering REST resources");
-        try{
-            e.jersey().register(new AdminController());
-            e.jersey().register(new ProfessorController());
-            e.jersey().register(new StudentController());
-            e.jersey().register(new UserController());
-        } catch (Exception exception) {
-            exception.printStackTrace();
-            System.out.println(exception);
-        }
+
+
+        e.jersey().register(new AdminController());
+        e.jersey().register(new ProfessorController());
+        e.jersey().register(new StudentController());
+        e.jersey().register(new UserController());
+
 
     }
 
     public static void main(String[] args) throws Exception {
-        System.out.println("main call");
-        new App().run(args);
+        LOGGER.info("main call");
+        new CRSDropWizardApplication().run(args);
     }
 }
